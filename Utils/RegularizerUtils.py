8f7d5989ac741ae592570f33548993359ae51790
githubanon123 <45709645+githubanon123@users.noreply.github.com>
2020-12-10 00:08:59 +0530

    Update RegularizerUtils.py

--- Utils/RegularizerUtils.py
+++ Utils/RegularizerUtils.py
@@ -1,18 +1,3 @@
-import numpy as np 
-
-def standardize (X):
-	X = np.asarray(X)
-	m = X.shape[1]
-	#mean = np.array(m)
-	#var = np.array(m)
-	for i in range (m):
-		mean = np.mean(X[:, i])
-		var = np.mean((X[:, i] - mean)**2)
-		X[:, i] = (X[:, i]-mean)/np.sqrt(var)
-	return X
-
-
-
 '''Shaurya's portion follows, go through once'''
 
 import numpy as np
